a53782de8f6a8dd1c255b1fb2cacdf3d5b8a68e9
Karishma112 <Karishma.Patil@swatsan.com>
2024-11-14 09:17:19 +0530

    companycard

--- Src/Constant/CustomCompanyCard.js
+++ Src/Constant/CustomCompanyCard.js
@@ -4,14 +4,14 @@ import {IconButton} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialIcons'; // Import Material Icons for stars
 import {useNavigation} from '@react-navigation/native';
 import {colors} from '../Global_CSS/TheamColors';
-
+ 
 const CustomCompanyCard = ({jobData}) => {
   const navigation = useNavigation();
-
+ 
   if (!jobData || typeof jobData !== 'object') {
     return <Text style={styles.errorText}>Invalid job data</Text>;
   }
-
+ 
   // Function to render stars based on rating
   const renderStars = rating => {
     const stars = [];
@@ -27,7 +27,7 @@ const CustomCompanyCard = ({jobData}) => {
     }
     return stars;
   };
-
+ 
   return (
     <View style={styles.companyContainer}>
       <TouchableOpacity
@@ -44,42 +44,22 @@ const CustomCompanyCard = ({jobData}) => {
         <View>
           <Text style={styles.companyName}>{jobData.company.company_name}</Text>
         </View>
-
+ 
         {/* Display Company Rating as Stars */}
         <View style={styles.ratingContainer}>
           {renderStars(Math.round(jobData.company.rating))}
         </View>
-<<<<<<< HEAD
-          {/* <View> */}
+ 
         <Text style={styles.mnctext}>Foreign MNC</Text>
-        {/* </View> */}
-        <Text style={{color:'blue',textAlign:'center'}}>View jobs</Text>
-=======
-
-        <Text style={styles.mnctext}>Foreign MNC</Text>
-
+ 
         <Text style={{color: 'blue', textAlign: 'center'}}>View jobs</Text>
->>>>>>> a4255f9c613925c72ef8cdbfbe63b7a06d91de10
       </TouchableOpacity>
     </View>
   );
 };
-
+ 
 const styles = StyleSheet.create({
   companyContainer: {
-<<<<<<< HEAD
-    padding: 10,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    marginRight: 8,
-  //  alignContent:'center',
-  justifyContent:'center',
-  // alignSelf:'center',
-    // borderWidth: 1,            // Set border width
-    // borderColor: '#ccc',   
-
-
-=======
     width:'100%',
     // maxWidth:
     padding: 10,
@@ -89,9 +69,8 @@ const styles = StyleSheet.create({
     //  alignContent:'center',
     justifyContent: 'center',
     // alignSelf:'center',
-    // borderWidth: 1, 
+    // borderWidth: 1,
     // borderColor: '#ccc',
->>>>>>> a4255f9c613925c72ef8cdbfbe63b7a06d91de10
   },
   companyImage: {
     width: 42,
@@ -99,24 +78,14 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginRight: 10,
     // marginLeft:20,
-<<<<<<< HEAD
-    margin:10,
-    alignSelf:'center'
-    
-=======
     margin: 10,
     alignSelf: 'center',
->>>>>>> a4255f9c613925c72ef8cdbfbe63b7a06d91de10
   },
   companyName: {
     textAlign: 'center',
     fontSize: 12,
     color: 'gray',
-<<<<<<< HEAD
-    alignSelf:'center'
-=======
     alignSelf: 'center',
->>>>>>> a4255f9c613925c72ef8cdbfbe63b7a06d91de10
   },
   location: {
     flexDirection: 'row',
@@ -131,23 +100,9 @@ const styles = StyleSheet.create({
   ratingContainer: {
     flexDirection: 'row',
     marginVertical: 4,
-<<<<<<< HEAD
-    alignSelf:'center'
-=======
     alignSelf: 'center',
->>>>>>> a4255f9c613925c72ef8cdbfbe63b7a06d91de10
   },
-
-  mnctext: {
-    backgroundColor: colors.background,
-    color: colors.blackText,
-    fontSize: 12,
-    margin: 5,
-    padding: 5,
-    textAlign: 'center',
-    alignSelf: 'center',
-    borderRadius: 5,
-  },
+ 
   mnctext: {
     backgroundColor: colors.background,
     color: colors.blackText,
@@ -159,5 +114,5 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
 });
-
+ 
 export default CustomCompanyCard;
